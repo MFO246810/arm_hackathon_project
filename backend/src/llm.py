d2a01da8ab2ac545de91cadaad11da202ddb7794
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-13 10:14:10 -0600

    Minor changes

--- backend/src/llm.py
+++ backend/src/llm.py
@@ -2,10 +2,10 @@ from ollama import chat
 from ollama import ChatResponse
 
 def query_model(Name, query):
-    response: ChatResponse = chat(model='gemma3', messages=[{
+    response: ChatResponse = chat(model=Name, messages=[{
         'role': 'user',
-        'content': 'Why is the sky blue?',
+        'content': query,
     },])
-    
+
     return response['message']['content']
 
