797f9059ff79de1e4a43ecf8db2b96ceec938b18
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-13 08:54:29 -0600

    Minor changes

--- backend/test/app_test.py
+++ backend/test/app_test.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 import pytest
 from src.models import MODELS
 from src.llm import query_model
