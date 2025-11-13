0b325f9656e0148f616d4754b018a0b72dedb1e1
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-13 09:44:27 -0600

    Minor changes

--- backend/src/llm.py
+++ backend/src/llm.py
@@ -1,9 +1,9 @@
 import ollama
 
-def query_model(Name, Query):
-    response = ollama.chat(model=Name, messages=[
-        {'role': 'user', 'content': Query}
-    ])
-
-    return response
+def query_model(Name, query):
+    response = ollama.chat(
+        model=Name,
+        messages=[{'role': 'user', 'content': query}]
+    )
+    return response['message']['content']
 
