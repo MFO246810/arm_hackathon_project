e889117af5f25b082d5d9fc96c7a576e12abbe7f
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-13 11:25:29 -0600

    Minor Changes

--- backend/src/models.py
+++ backend/src/models.py
@@ -3,6 +3,6 @@ from enum import Enum
 class MODELS(Enum):
     GEMMA = "Gemma3n:e2b"
     LLAMA = "llama3.2:1b"
-    DEEPSEEK = "deepseek-r1:1.5b "
+    DEEPSEEK = "deepseek-r1:1.5b"
     GRANITE = "Granite3.3:2b"
     PHI = "phi3:mini"
