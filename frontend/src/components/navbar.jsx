52cb4f19048e9109999a5606c1c6c605c0032088
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-13 20:38:56 -0600

    Backend fully tested

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import "./navbar.css"
+import { h } from 'preact';
+
+export default function Navbar(){
+    return(
+        <>
+            <nav className="navbar-container">
+                <div className="Navbar-left">
+                    Sidebar button
+                </div>
+                <div className="Navbar-left">
+                    Sidebar button
+                </div>
+                <div className="Navbar-left">
+                    Sidebar button
+                </div>
+            </nav>
+        </>
+    )
+}
