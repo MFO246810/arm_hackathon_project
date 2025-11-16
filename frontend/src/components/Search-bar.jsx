5b762764cc296d8527f439c4f54027f0edb3d2e4
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-15 23:13:19 -0600

    Navbar Implemented

---
+++ frontend/src/components/Search-bar.jsx
@@ -0,0 +1,13 @@
+import "./search-bar.css"
+
+export default function search_bar(){
+    return(
+        <>
+            <div>
+                <form>
+                    
+                </form>
+            </div>
+        </>
+    )
+}
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,19 +1,29 @@
 import "./navbar.css"
 import { h } from 'preact';
+import { Link } from 'preact-router/match';
 
 export default function Navbar(){
     return(
         <>
-            <nav className="navbar-container">
-                <div className="Navbar-left">
-                    Sidebar button
+            <nav class="navbar">
+                <div class="navbar-container">
+                    <div class="navbar-item">
+                    </div>
+                    <div class="navbar-item">
+                        <div class="navbar-item-center">
+                            <h2 class="navbar-item-center-title"> LLM Tester</h2>
+                        </div>
+                    </div>
+                    <div class="navbar-item">
+                        <div class="navbar-item-right">
+                            <div class="navbar-item-right-link-container">
+                                <Link activeClassName="active" href="/" class="navbar-item-right-link">Query</Link>
+                                <Link activeClassName="active" href="/review" class="navbar-item-right-link">Review</Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="Navbar-left">
-                    Sidebar button
-                </div>
-                <div className="Navbar-left">
-                    Sidebar button
-                </div>
+                
             </nav>
         </>
     )
