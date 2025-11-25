46405624de6f1932cc7957bd3d9d5d8f59d7c624
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-25 14:14:34 -0600

    Minor changes

--- frontend/src/components/display_response.jsx
+++ frontend/src/components/display_response.jsx
@@ -8,8 +8,8 @@ export default function Display_response({prop}){
         <>
             <div class="Response_Container">
                 <div class="Response_Title">
-                    <h3> Model Used: {prop.model_used}</h3>
-                    <p> <strong> User Query: </strong> {prop.model_query}</p>
+                    {prop.model_used && <h3> Model Used: {prop.model_used}</h3>}
+                    {prop.model_query && <p> <strong> User Query: </strong> {prop.model_query}</p>}
                     <div class="Response_Body">
                         <ReactMarkdown> 
                             {prop.model_response}
