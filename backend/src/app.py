1700cba4d16611d90a59278f93d9ff6e8d925f18
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-11-27 11:12:12 -0600

    Minor changes

--- backend/src/app.py
+++ backend/src/app.py
@@ -118,7 +118,7 @@ def get_DB_data():
     
     db = SessionLocal()
 
-    results = db.execute(select(QueryData)).scalars().all()
+    results = db.execute(select(Query_Data)).scalars().all()
 
     db.close()
 
